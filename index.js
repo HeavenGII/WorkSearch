6423355f68d1433ebeac3d9349934d85daadf5f8
Heaven <i.golovatski@mail.ru>
2025-05-20 01:42:20 +0300

    Init commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const cookieParser = require('cookie-parser')
 const csrf = require('csurf')
 const csrfProtection = csrf({ cookie: true })
 const flash = require('connect-flash')
